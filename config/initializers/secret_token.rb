7b033ca50ebb6aa0631441af37f74b712a38d913
PHAM MINH NHAT <pmnhatdn@gmail.com>
2013-04-13 14:16:52 -0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testror::Application.config.secret_token = 'b0dab2f5afb9b0d3bdf6bb00057c513611a4f7034e7b4dff5fbfd7b09013a14bfcd834bafc78976294e0e7b94590b7ccf54a8d1a931b6efd5768047942ede516'
